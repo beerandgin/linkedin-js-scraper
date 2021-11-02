fc2dfe7423b7f334f8ffcb51a2433c8c75a29f7d
Douglas Berdeaux <weaknetlabs@gmail.com>
2021-11-02 11:53:31 -0400

    Create ljs.js

---
+++ ljs.js
@@ -0,0 +1,9 @@
+// LinkedIn Employee Name JS Scraper Tool,
+// 2021 - Douglas Berdeaux
+// rackunsec@gmail.com
+var employees = document.getElementsByClassName("org-people-profile-card__profile-title");
+const employees_array = [];
+for (var i=0; i<employees.length; i++) {
+   employees_array.push(employees[i].innerHTML.replace(/(\r\n|\n|\r)/gm, "").replace(/<.*/,''));
+}
+console.log(JSON.stringify(employees_array));
