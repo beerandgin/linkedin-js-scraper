27e511b40e4272679ebfdd1319f7c7be18eae7f6
Douglas Berdeaux <weaknetlabs@gmail.com>
2021-11-02 12:00:24 -0400

    Removed leading spaces in names using RegExp

--- ljs.js
+++ ljs.js
@@ -4,6 +4,6 @@
 var employees = document.getElementsByClassName("org-people-profile-card__profile-title");
 const employees_array = [];
 for (var i=0; i<employees.length; i++) {
-   employees_array.push(employees[i].innerHTML.replace(/(\r\n|\n|\r)/gm, "").replace(/<.*/,''));
+   employees_array.push(employees[i].innerHTML.replace(/(\r\n|\n|\r)/gm, "").replace(/<.*/,'').replace(/^\s+/,''));
 }
 console.log(JSON.stringify(employees_array));
